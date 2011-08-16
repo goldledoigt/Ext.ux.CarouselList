63d69d0164f4e2fd28280a94bab96ecb058079ca
goldledoigt <gary@chewam.com>
2011-08-17 00:39:02 +0200

    add sources

---
+++ data.js
@@ -0,0 +1,81 @@
+var __DATA__ = [{
+    items: [{
+        html: '1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '3 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }]
+}, {
+    items: [{
+        html: '1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '3 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '4 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '5 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }]
+}, {
+    items: [{
+        html: '1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '3 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '4 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '5 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '6 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '7 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }]
+}, {
+    items: [{
+        html: '1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '3 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '4 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '5 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '6 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '7 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '8 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '9 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }]
+}, {
+    items: [{
+        html: '1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '3 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '4 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '5 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '6 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '7 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '8 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '9 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '10 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }, {
+        html: '11 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur...'
+    }]
+}];
